c10e67fff17b0ae470f792c483f4a60e5679fd84
Ofek <ofekp@users.noreply.github.com>
2017-10-30 01:16:20 +0200

    PORT_MAPPING_INVALID_ACTION

--- src/TinyUPnP.h
+++ src/TinyUPnP.h
@@ -19,6 +19,7 @@
 #define UPNP_SERVICE_TYPE "<serviceType>urn:schemas-upnp-org:service:WANPPPConnection:1</serviceType>"
 #define UPNP_SERVICE_TYPE_2 "<serviceType>urn:schemas-upnp-org:service:WANIPConnection:1</serviceType>"
 #define PORT_MAPPING_INVALID_INDEX "<errorDescription>SpecifiedArrayIndexInvalid</errorDescription>"
+#define PORT_MAPPING_INVALID_ACTION "<errorDescription>Invalid Action</errorDescription>"
 
 #define RULE_PROTOCOL_TCP "TCP"
 #define RULE_PROTOCOL_UDP "UDP"
