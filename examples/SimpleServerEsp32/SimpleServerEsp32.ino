4c5865dd41414b59efe4d506a944dfe395273e8a
Benjamin Balga <balga.benjamin@gmail.com>
2019-09-25 22:37:51 +0200

    Add ESP32 simple server example.

---
+++ examples/SimpleServerEsp32/SimpleServerEsp32.ino
@@ -0,0 +1,102 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <TinyUPnP.h>
+
+const char* ssid = "<FILL THIS!>";
+const char* password = "<FILL THIS!>";
+
+WebServer server(1900);
+
+#define LISTEN_PORT 1900  // http://<IP>:<LISTEN_PORT>/?name=<your string>
+#define LEASE_DURATION 36000  // seconds
+#define FRIENDLY_NAME "<FILL THIS!>"  // this name will appear in your router port forwarding section
+
+TinyUPnP tinyUPnP(20000);  // -1 means blocking, preferably, use a timeout value (ms)
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp32!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  boolean portMappingAdded = false;
+  tinyUPnP.addPortMappingConfig(WiFi.localIP(), LISTEN_PORT, RULE_PROTOCOL_TCP, LEASE_DURATION, FRIENDLY_NAME);
+  while (!portMappingAdded) {
+    portMappingAdded = tinyUPnP.commitPortMappings();
+    Serial.println("");
+  
+    if (!portMappingAdded) {
+      // for debugging, you can see this in your router too under forwarding or UPnP
+      tinyUPnP.printAllPortMappings();
+      Serial.println(F("This was printed because adding the required port mapping failed"));
+      delay(30000);  // 30 seconds before trying again
+    }
+  }
+  
+  Serial.println("UPnP done");
+  
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+    
+  tinyUPnP.updatePortMappings(600000);  // 10 minutes
+  
+  server.handleClient();
+  
+  delay(5);
+}
