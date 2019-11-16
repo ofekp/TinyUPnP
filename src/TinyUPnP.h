795cf4fa73eb2ae28baf61dbf89426fed6991b83
Ofek <ofekp@users.noreply.github.com>
2019-11-16 22:45:14 +0200

    code cleanups (#45)

--- src/TinyUPnP.h
+++ src/TinyUPnP.h
@@ -36,18 +36,18 @@ const String UPNP_SERVICE_TYPE_TAG_END = "</serviceType>";
 // TODO: idealy the SOAP actions should be verified as supported by the IGD before they are used
 // 		 a struct can be created for each action and filled when the XML descriptor file is read
 /*const String SOAPActions [] = {
-	"AddPortMapping",
-	"GetSpecificPortMappingEntry",
-	"DeletePortMapping",
-	"GetGenericPortMappingEntry",
-	"GetExternalIPAddress"
+    "AddPortMapping",
+    "GetSpecificPortMappingEntry",
+    "DeletePortMapping",
+    "GetGenericPortMappingEntry",
+    "GetExternalIPAddress"
 };*/
 
 /*
 #define SOAP_ERROR_TAG "errorDescription";
 const String SOAPErrors [] = {
-	"SpecifiedArrayIndexInvalid",
-	"Invalid Action"
+    "SpecifiedArrayIndexInvalid",
+    "Invalid Action"
 };*/
 
 /*
@@ -56,90 +56,90 @@ enum soapActionResult {
 }*/
 
 typedef struct _SOAPAction {
-	const char *name;
+    const char *name;
 } SOAPAction;
 
 typedef void (*callback_function)(void);
 
 typedef struct _gatewayInfo {
-	// router info
-	IPAddress host;
-	int port;  // this port is used when getting router capabilities and xml files
-	String path;  // this is the path that is used to retrieve router information from xml files
-	
-	// info for actions
-	int actionPort;  // this port is used when performing SOAP API actions
-	String actionPath;  // this is the path used to perform SOAP API actions
-	String serviceTypeName;  // i.e "WANPPPConnection:1" or "WANIPConnection:1"
+    // router info
+    IPAddress host;
+    int port;  // this port is used when getting router capabilities and xml files
+    String path;  // this is the path that is used to retrieve router information from xml files
+    
+    // info for actions
+    int actionPort;  // this port is used when performing SOAP API actions
+    String actionPath;  // this is the path used to perform SOAP API actions
+    String serviceTypeName;  // i.e "WANPPPConnection:1" or "WANIPConnection:1"
 } gatewayInfo;
 
 typedef struct _upnpRule {
-	int index;
-	String devFriendlyName;
-	IPAddress internalAddr;
-	int internalPort;
-	int externalPort;
-	String protocol;
-	int leaseDuration;
+    int index;
+    String devFriendlyName;
+    IPAddress internalAddr;
+    int internalPort;
+    int externalPort;
+    String protocol;
+    int leaseDuration;
 } upnpRule;
 
 typedef struct _upnpRuleNode {
-	_upnpRule *upnpRule;
-	_upnpRuleNode *next;
+    _upnpRule *upnpRule;
+    _upnpRuleNode *next;
 } upnpRuleNode;
 
 enum portMappingResult {
-	SUCCESS,  // port mapping was added
-	ALREADY_MAPPED,  // the port mapping is already found in the IGD
-	EMPTY_PORT_MAPPING_CONFIG,
-	NETWORK_ERROR,
-	TIMEOUT,
-	NOP  // the check is delayed
+    SUCCESS,  // port mapping was added
+    ALREADY_MAPPED,  // the port mapping is already found in the IGD
+    EMPTY_PORT_MAPPING_CONFIG,
+    NETWORK_ERROR,
+    TIMEOUT,
+    NOP  // the check is delayed
 };
 
 class TinyUPnP
 {
-	public:
-		TinyUPnP(unsigned long timeoutMs);
-		~TinyUPnP();
-		// when the ruleIP is set to the current device IP, the IP of the rule will change if the device changes its IP
-		// this makes sure the traffic will be directed to the device even if the IP chnages
-		void addPortMappingConfig(IPAddress ruleIP /* can be NULL */, int rulePort, String ruleProtocol, int ruleLeaseDuration, String ruleFriendlyName);
-		portMappingResult commitPortMappings();
-		portMappingResult updatePortMappings(unsigned long intervalMs, callback_function fallback = NULL /* optional */);
-		boolean printAllPortMappings();
-		void printPortMappingConfig();  // prints all the port mappings that were added using `addPortMappingConfig`
-		boolean testConnectivity(unsigned long startTime = 0);
-	private:
-		boolean connectUDP();
-		void broadcastMSearch();
-		boolean waitForUnicastResponseToMSearch(gatewayInfo *deviceInfo, IPAddress gatewayIP);
-		boolean getGatewayInfo(gatewayInfo *deviceInfo, long startTime);
-		boolean isGatewayInfoValid(gatewayInfo *deviceInfo);
-		void clearGatewayInfo(gatewayInfo *deviceInfo);
-		boolean connectToIGD(IPAddress host, int port);
-		boolean getIGDEventURLs(gatewayInfo *deviceInfo);
-		boolean addPortMappingEntry(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
-		boolean verifyPortMapping(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
-		boolean deletePortMapping(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
-		boolean applyActionOnSpecificPortMapping(SOAPAction *soapAction, gatewayInfo *deviceInfo, upnpRule *rule_ptr);
-		void removeAllPortMappingsFromIGD();
-		//char* ipAddressToCharArr(IPAddress ipAddress);  // ?? not sure this is needed
-		void upnpRuleToString(upnpRule *rule_ptr);
-		String getSpacesString(int num);
-		IPAddress getHost(String url);
-		int getPort(String url);
-		String getPath(String url);
-		String getTagContent(const String &line, String tagName);
+    public:
+        TinyUPnP(unsigned long timeoutMs);
+        ~TinyUPnP();
+        // when the ruleIP is set to the current device IP, the IP of the rule will change if the device changes its IP
+        // this makes sure the traffic will be directed to the device even if the IP chnages
+        void addPortMappingConfig(IPAddress ruleIP /* can be NULL */, int rulePort, String ruleProtocol, int ruleLeaseDuration, String ruleFriendlyName);
+        portMappingResult commitPortMappings();
+        portMappingResult updatePortMappings(unsigned long intervalMs, callback_function fallback = NULL /* optional */);
+        boolean printAllPortMappings();
+        void printPortMappingConfig();  // prints all the port mappings that were added using `addPortMappingConfig`
+        boolean testConnectivity(unsigned long startTime = 0);
+    private:
+        boolean connectUDP();
+        void broadcastMSearch();
+        boolean waitForUnicastResponseToMSearch(gatewayInfo *deviceInfo, IPAddress gatewayIP);
+        boolean getGatewayInfo(gatewayInfo *deviceInfo, long startTime);
+        boolean isGatewayInfoValid(gatewayInfo *deviceInfo);
+        void clearGatewayInfo(gatewayInfo *deviceInfo);
+        boolean connectToIGD(IPAddress host, int port);
+        boolean getIGDEventURLs(gatewayInfo *deviceInfo);
+        boolean addPortMappingEntry(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
+        boolean verifyPortMapping(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
+        boolean deletePortMapping(gatewayInfo *deviceInfo, upnpRule *rule_ptr);
+        boolean applyActionOnSpecificPortMapping(SOAPAction *soapAction, gatewayInfo *deviceInfo, upnpRule *rule_ptr);
+        void removeAllPortMappingsFromIGD();
+        //char* ipAddressToCharArr(IPAddress ipAddress);  // ?? not sure this is needed
+        void upnpRuleToString(upnpRule *rule_ptr);
+        String getSpacesString(int num);
+        IPAddress getHost(String url);
+        int getPort(String url);
+        String getPath(String url);
+        String getTagContent(const String &line, String tagName);
 
-		/* members */
-		upnpRuleNode *_headRuleNode;
-		unsigned long _lastUpdateTime;
-		unsigned long _timeoutMs;  // 0 for blocking operation
-		WiFiUDP _udpClient;
-		WiFiClient _wifiClient;
-		gatewayInfo _gwInfo;
-		unsigned long _consequtiveFails;
+        /* members */
+        upnpRuleNode *_headRuleNode;
+        unsigned long _lastUpdateTime;
+        unsigned long _timeoutMs;  // 0 for blocking operation
+        WiFiUDP _udpClient;
+        WiFiClient _wifiClient;
+        gatewayInfo _gwInfo;
+        unsigned long _consequtiveFails;
 };
 
 #endif
